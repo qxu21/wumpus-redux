7001ecd2525617598b15e635e4daa43d75d06037
qxu21 <qqxu21@gmail.com>
2020-05-27 21:37:49 -0700

    it maybe work?

--- wumpus.py
+++ wumpus.py
@@ -5,57 +5,63 @@ import config
 
 
 class Wumpus(commands.Bot):
-    def __init__(self, **kwargs):
+    def __init__(self, db, preps):
         super().__init__(
             command_prefix="w"
         )
-        self.db = kwargs.pop("db")
+        self.db = db
+        for (key, val) in preps.items():
+            setattr(self ,key ,val)
         
-        self.db_start_insert = self.db.prepare("""
-            insert into words (id, after, count) values
-            ($1, $2, 1)
-            on conflict do update 
-            set count=count + 1;
-        """)
-        self.db_word_insert = self.db.prepare("""
-            insert into words (id, before, after, count) values
-            ($1, $2, $3, 1)
-            on conflict do update 
-            set count=count + 1;
-        """)
-        self.db_end_insert = self.db.prepare("""
-            insert into words (id, before, count) values
-            ($1, $2, 1)
-            on conflict do update 
-            set count=count + 1;
-        """)
-        self.db_progress = self.db.prepare("""
-            insert into progress (channel_id, message_id) values
-            ($1,$2)
-            on conflict do update
-            set message_id=$2;
-        """)
         self.remove_command("help")
+        self.add_command(build)
 
 async def run(token):
     db = await asyncpg.create_pool(**config.dbc)
     await db.execute("""
         CREATE TABLE IF NOT EXISTS words(
-            id varchar,
+            id varchar not null,
             before varchar,
             after varchar,
-            count integer,
+            count integer not null,
             primary key (id, before, after)
         );
     """)
 
     await db.execute("""
         create table if not exists progress(
-            channel_id bigint not null primary key,
+            channel_id bigint primary key,
             message_id bigint not null
         );
     """)
-    wumpus = Wumpus(db=db)
+    async with db.acquire() as conn:
+        preps = {
+            "db_start_insert": await conn.prepare("""
+                insert into words (id, before, after, count) values
+                ($1, NULL, $2, 1)
+                on conflict (id, before, after) do update 
+                set count=words.count + 1;
+            """),
+            "db_word_insert": await conn.prepare("""
+                insert into words (id, before, after, count) values
+                ($1, $2, $3, 1)
+                on conflict (id, before) do update 
+                set count=words.count + 1;
+            """),
+            "db_end_insert": await conn.prepare("""
+                insert into words (id, before, after, count) values
+                ($1, $2, NULL, 1)
+                on conflict (id, before) do update 
+                set count=words.count + 1;
+            """),
+            "db_progress": await conn.prepare("""
+                insert into progress (channel_id, message_id) values
+                ($1, $2)
+                on conflict (channel_id) do update
+                set message_id=$2;
+            """)
+        }
+    wumpus = Wumpus(db=db,preps=preps)
     try:
         await wumpus.start(token)
     except KeyboardInterrupt:
@@ -67,23 +73,24 @@ def getuserid(message, ctx):
 
 @commands.command()
 async def build(ctx):
-    for channel in ctx.guild.text_channels[1]: #TESTING - REMOVE [1]
+    for channel in ctx.guild.text_channels[0]: #TESTING - REMOVE [1]
         after = None
         while True:
             async for message in channel.history(limit=100,after=after,oldest_first=True):
                 words = message.split()
                 userid = getuserid(message, channel)
                 #insert the first word of the message
-                await db_start_insert.fetch(userid,words[0])
-                await db_end_insert.fetch(userid,words[-1])
+                await ctx.bot.db_start_insert.fetch(userid,words[0])
+                await ctx.bot.db_end_insert.fetch(userid,words[-1])
                 for (index, word) in enumerate(words[:-1]):
-                    await db_word_insert.fetch(userid,word,words[index+1])
+                    await ctx.bot.db_word_insert.fetch(userid,word,words[index+1])
                 after = message
+            await ctx.send(f"Made it to {after.clean_content}")
             break #TESTING
             if after.id == channel.last_message_id:
                 break
-            db_progress.fetch(channel.id,after.id)
-        await db.execute("UPDATE progress SET message_id=0 WHERE channel_id=$1",channel.id)
+            ctx.bot.db_progress.fetch(channel.id, after.id)
+        await ctx.bot.db.execute("UPDATE progress SET message_id=0 WHERE channel_id=$1",channel.id)
         
 
 loop = asyncio.get_event_loop()
